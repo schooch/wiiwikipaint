6c0551d80fc55fcaef68f772fbe70b3d96a139fa
Ian Lainchbury <Ian.Lainchbury@turner.com>
2014-05-12 15:17:23 +0100

    Tidying up jslint files

--- Jakefile.js
+++ Jakefile.js
@@ -1,7 +1,33 @@
+/*global desc, task, jake, fail, complete */
+"use strict";
+
 task('default', ['lint']);
 
 desc('lint everything');
-task('lint', [], function(){
+task('lint', [], function() {
   var lint = require('./build/lint/lint_runner.js');
-  lint.validateFile('jakeFile.js', {}, {});
+
+  var files = new jake.FileList();
+  files.include('**/*.js');
+  files.exclude('node_modules');
+
+  var options = {
+    bitwise: true,
+    curly: false,
+    eqeqeq: true,
+    forin: true,
+    immed: true,
+    latedef: true,
+    newcap: true,
+    noarg: true,
+    noempty: true,
+    nonew: true,
+    regexp: true,
+    undef: true,
+    strict: true,
+    trailing: true,
+    node: true
+  };
+
+  lint.validateFileList(files.toArray(), options, {});
 });
