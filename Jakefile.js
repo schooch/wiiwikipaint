7b3fdabfc573e75b8912861538cb240dc21f921c
Ian Lainchbury <Ian.Lainchbury@turner.com>
2014-05-12 15:33:41 +0100

    Adding strict lint functions

--- Jakefile.js
+++ Jakefile.js
@@ -1,33 +1,37 @@
-/*global desc, task, jake, fail, complete */
-"use strict";
+(function() {
+  /*global desc, task, jake, fail, complete */
+  "use strict";
 
-task('default', ['lint']);
+  task('default', ['lint']);
 
-desc('lint everything');
-task('lint', [], function() {
-  var lint = require('./build/lint/lint_runner.js');
+  desc('lint everything');
+  task('lint', [], function() {
+    var lint = require('./build/lint/lint_runner.js');
 
-  var files = new jake.FileList();
-  files.include('**/*.js');
-  files.exclude('node_modules');
+    var files = new jake.FileList();
+    files.include('**/*.js');
+    files.exclude('node_modules');
 
-  var options = {
-    bitwise: true,
-    curly: false,
-    eqeqeq: true,
-    forin: true,
-    immed: true,
-    latedef: true,
-    newcap: true,
-    noarg: true,
-    noempty: true,
-    nonew: true,
-    regexp: true,
-    undef: true,
-    strict: true,
-    trailing: true,
-    node: true
-  };
+    lint.validateFileList(files.toArray(), nodeLintOptions(), {});
+  });
 
-  lint.validateFileList(files.toArray(), options, {});
-});
+  function nodeLintOptions(){
+    return {
+      bitwise: true,
+      curly: false,
+      eqeqeq: true,
+      forin: true,
+      immed: true,
+      latedef: true,
+      newcap: true,
+      noarg: true,
+      noempty: true,
+      nonew: true,
+      regexp: true,
+      undef: true,
+      strict: true,
+      trailing: true,
+      node: true
+    };
+  }
+}());
