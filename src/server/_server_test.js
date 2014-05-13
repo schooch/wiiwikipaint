fbf379ec67b9a8d5090416177791b45b407b6202
Ian Lainchbury <Ian.Lainchbury@turner.com>
2014-05-13 16:28:48 +0100

    Addded testing of get requests on server

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -4,10 +4,17 @@
 var server = require('./server.js');
 var http = require('http');
 
-exports.testHttpServer = function(test){
-  server.start();
-  http.get('http://localhost:8080', function(response){
-
+exports.tearDown = function(done){
+  server.stop(function(){
+    done();
   });
-  test.done();
+};
+
+exports.testServerRespondsToGetRequests = function(test){
+  server.start();
+
+  http.get('http://localhost:8080', function(response){
+    response.on('data', function(){});
+    test.done();
+  });  
 };
--- src/server/server.js
+++ src/server/server.js
@@ -2,18 +2,18 @@
 "use strict";
 
 var http = require('http');
+var server;
 
 exports.start = function(){
-  var server = http.createServer();
+  server = http.createServer();
 
   server.on('request', function(request, response){
-    console.log('recieved request');
-
-    //var body = "<html><head><title>Node Server</title></head><body><h1>This is node's HTTP server</h1></body></html>";
-
-    response.end('foo');
+    response.end();
   });
-  server.listen(8080);
 
-  console.log('Started :)');
+  server.listen(8080);
+};
+
+exports.stop = function(callback){
+  server.close(callback);
 };
