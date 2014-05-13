46ee5d35793ba171ee85dc0b3e384d6a9c84f845
Ian Lainchbury <Ian.Lainchbury@turner.com>
2014-05-13 16:34:26 +0100

    Addded testing of get requests on server

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -10,11 +10,21 @@ exports.tearDown = function(done){
   });
 };
 
-exports.testServerRespondsToGetRequests = function(test){
+exports.test_serverRespondsToGetRequests = function(test){
   server.start();
 
   http.get('http://localhost:8080', function(response){
     response.on('data', function(){});
     test.done();
   });  
+};
+
+exports.test_serverReturnesHelloWorld = function(test){
+  server.start(); // TODO remove duplocation
+  var request = http.get('http://localhost:8080');
+  request.on('response', function(response){
+    response.on('data', function(){});
+    test.equal(response.statusCode, 200, 'Status code 200');
+    test.done();
+  });
 };
