985809fe6545bdcbb1e1dd7cc3d87194ba48d693
Ian Lainchbury <Ian.Lainchbury@turner.com>
2014-05-22 09:24:39 +0100

    tidying up code

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -1,24 +1,32 @@
-/*global desc, task, jake, fail, complete, chai, expect, describe, it, dump, wwp, Raphael, $ */
+/*global desc, task, jake, fail, complete, chai, expect, beforeEach, afterEach, describe, it, dump, wwp, Raphael, $ */
 
 (function(){
   "use strict";
 
   var width = 400;
-  var height = 200;
+  var height = 400;
   var paperId = 'wwp-drawing-area';
   var drawingAreaDiv = '<div id="' +paperId+ '" style="border: solid 1px red;"></div>';
   var type;
 
   describe('Drawing area', function(){
+    var drawingArea;
+    var paper;
+
+    beforeEach(function(){
+      drawingArea = $(drawingAreaDiv);
+      $(document.body).append(drawingArea);
+
+      // Initialize drawing area
+      paper = wwp.initializeDrawingArea(drawingArea[0]);
+    });
+
+    afterEach(function(){
+      drawingArea.remove();
+    });
+
     it('should be initialized in pre-defined div', function(){
-      // Create div
-      var div = $(drawingAreaDiv);
-      $(document.body).append(div);
-
-      // Verify initalized correctly
-      wwp.initializeDrawingArea(div[0]);
-
-      var tagName = $(div).children()[0].tagName.toLowerCase();
+      var tagName = $(drawingArea).children()[0].tagName.toLowerCase();
 
       // verify initialized corretly
       if(Raphael.svg){
@@ -34,23 +42,14 @@
     });
 
     it('should have the same dimensions as the enclosing div', function(){
-      // Create div
-      var div = $(drawingAreaDiv);
-      $(document.body).append(div);
-
-      // Initialize div
-
       // Verify initalized correctly
-      var paper = wwp.initializeDrawingArea(div[0], width, height);
+      paper = wwp.initializeDrawingArea(drawingArea[0], width, height);
       expect(paper.width).to.equal(width);
       expect(paper.height).to.equal(height);
     });
 
     it('should draw a line', function(){
-      var div = $(drawingAreaDiv);
-      $(document.body).append(div);
-
-      var paper = wwp.initializeDrawingArea(div[0]);
+      paper = wwp.initializeDrawingArea(drawingArea[0]);
       var startX = 20;
       var startY = 30;
       var finishX = 30;
